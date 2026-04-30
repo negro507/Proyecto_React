7d4161b56eea4c232a21162a58d4ab9fd67d62f2
carlos andres moreno <morenoyepes669@gmail.com>
2026-04-30 01:14:09 -0500

    Mi primer commit

---
+++ client/src/Js/App.js
@@ -0,0 +1,13 @@
+import '../Css/App.css';
+
+function App() {
+  alert('Welcome to the MERN Stack Application!');
+
+  return (
+    <div className="App">
+      <h1>Welcome to the MERN Stack Application!</h1>
+    </div>
+  );
+}
+
+export default App;
